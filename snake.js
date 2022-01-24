6dcc9ed480c111ef0e5d4378aa21c098cf5b1bbc
Jinwook <6kimjinwook@gmail.com>
2022-01-24 17:55:30 +0900

    head direction

---
+++ snake.js
@@ -0,0 +1,25 @@
+const head = document.getElementById("head");
+const KEY_RIGHT = "ArrowRight"
+const KEY_DOWM = "ArrowDown"
+const KEY_LEFT = "ArrowLeft"
+const KEY_UP = "ArrowUp"
+function handleHeadMove(e) {
+    switch (e.key) {
+        case KEY_RIGHT:
+            head.innerText = "🢂";
+            break
+        case KEY_DOWM:
+            head.innerText = "🢃";
+            break
+        case KEY_LEFT:
+            head.innerText = "🢀";
+            break
+        case KEY_UP:
+            head.innerText = "🢁";
+            break
+    }
+}
+
+
+window.addEventListener("keydown", handleHeadMove);
+
