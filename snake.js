0b02c57949893e81bf6b54168256bb70618b59eb
Jinwook <6kimjinwook@gmail.com>
2022-01-28 17:37:36 +0900

    turnMechanism

--- snake.js
+++ snake.js
@@ -1,8 +1,12 @@
+const snake = document.querySelector(".snake");
 const head = document.querySelector(".head");
 const KEY_RIGHT = "ArrowRight"
-const KEY_DOWM = "ArrowDown"
+const KEY_DOWN = "ArrowDown"
 const KEY_LEFT = "ArrowLeft"
 const KEY_UP = "ArrowUp"
+
+const dx = 50;
+const dy = 50;
 
 function clearDirection(baba) {
     baba.classList.remove("right");
@@ -14,24 +18,41 @@ function clearDirection(baba) {
 function handleHeadMove(e) {
     switch (e.key) {
         case KEY_RIGHT:
+            if (head.classList.contains("left") || head.classList.contains("right")) {
+                break
+            };
             clearDirection(head);
             head.classList.add("right")
             break
-        case KEY_DOWM:
+        case KEY_DOWN:
+            if (head.classList.contains("up") || head.classList.contains("down")) {
+                break
+            };
             clearDirection(head);
             head.classList.add("down")
             break
         case KEY_LEFT:
+            if (head.classList.contains("right") || head.classList.contains("left")) {
+                break
+            };
             clearDirection(head);
             head.classList.add("left")
             break
         case KEY_UP:
+            if (head.classList.contains("down") || head.classList.contains("up")) {
+                break
+            };
             clearDirection(head);
             head.classList.add("up")
             break
     }
 }
 
+function snakeMove() {
+    
+}
+
+setInterval(snakeMove, 1000);
 
 window.addEventListener("keydown", handleHeadMove);
 
