8c7d9a4f34997fd4e3c2bc8c499258e649af30ed
Jinwook <6kimjinwook@gmail.com>
2022-01-28 09:01:25 +0900

    apple

--- snake.js
+++ snake.js
@@ -1,21 +1,33 @@
-const head = document.getElementById("head");
+const head = document.querySelector(".head");
 const KEY_RIGHT = "ArrowRight"
 const KEY_DOWM = "ArrowDown"
 const KEY_LEFT = "ArrowLeft"
 const KEY_UP = "ArrowUp"
+
+function clearDirection(baba) {
+    baba.classList.remove("right");
+    baba.classList.remove("down");
+    baba.classList.remove("left");
+    baba.classList.remove("up");
+}
+
 function handleHeadMove(e) {
     switch (e.key) {
         case KEY_RIGHT:
-            head.innerText = "→";
+            clearDirection(head);
+            head.classList.add("right")
             break
         case KEY_DOWM:
-            head.innerText = "↓";
+            clearDirection(head);
+            head.classList.add("down")
             break
         case KEY_LEFT:
-            head.innerText = "←";
+            clearDirection(head);
+            head.classList.add("left")
             break
         case KEY_UP:
-            head.innerText = "↑";
+            clearDirection(head);
+            head.classList.add("up")
             break
     }
 }
