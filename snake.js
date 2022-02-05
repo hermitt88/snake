666da4a49c6b5724abb1d739214164230156fd64
hermitt88 <6kimjinwook@gmail.com>
2022-02-05 21:35:39 +0900

    snake movement

--- snake.js
+++ snake.js
@@ -1,77 +1,108 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+const canvasW = canvas.width;
+const canvasH = canvas.height;
+
+const KEY_RIGHT = "ArrowRight"
+const KEY_DOWN = "ArrowDown"
+const KEY_LEFT = "ArrowLeft"
+const KEY_UP = "ArrowUp"
+
+const dx = 20;
+const dy = 20;
 
 const form = document.querySelector("form");
 
-ctx.fillStyle = "black";
-let x = 0;
-let y = 0;
+let x = 180;
+let y = 180;
+let snakeInterval = 300;
+let direction = "right";
+ctx.fillStyle = "green";
+paintSnakeBlock(x, y);
 
-function handleMouseMove(e) {
-    x = e.offsetX;
-    y = e.offsetY;
-    ctx.fillRect(x-9, y-9, 18, 18);
+window.onload = snakeGame();
+
+function snakeGame() {
+    setInterval(moveSnake, snakeInterval);
+}
+
+function moveSnake() {
+    if (x > 0 && y > 0 && x < canvasW - dx && y < canvasH - dy) {
+        removeSnakeBlock(x, y);
+        switch (direction) {
+            case "right":
+                x += dx;
+                paintSnakeBlock(x, y);
+                break
+            case "down":
+                y += dy;
+                paintSnakeBlock(x, y);
+                break
+            case "left":
+                x -= dx;
+                paintSnakeBlock(x, y);
+                break
+            case "up":
+                y -= dy;
+                paintSnakeBlock(x, y);
+                break
+        }
+    } else {
+        clearInterval(moveSnake);
+    }
+
+}
+
+function initialSetting() {
+    ctx.fillStyle = "green";
+    x = 180;
+    y = 180;
+    direction = "right";
+    paintSnakeBlock(x, y);
+}
+
+function paintSnakeBlock(x, y) {
+    ctx.fillRect(x+1, y+1, 18, 18);
+}
+
+function removeSnakeBlock(x, y) {
+    ctx.clearRect(x, y, 20, 20);
 }
 
 function handleClear(e) {
     e.preventDefault();
-    ctx.clearRect(0, 0, 400, 400);
-    ctx.beginPath();
+    ctx.clearRect(0, 0, canvasW, canvasH);
+    initialSetting();
 }
 
-canvas.addEventListener("mousemove", handleMouseMove);
 form.addEventListener("submit", handleClear);
 
+window.addEventListener("keydown", handleHeadMove);
 
-// const snake = document.querySelector(".snake");
-// const head = document.querySelector(".head");
-// const KEY_RIGHT = "ArrowRight"
-// const KEY_DOWN = "ArrowDown"
-// const KEY_LEFT = "ArrowLeft"
-// const KEY_UP = "ArrowUp"
-
-// const dx = 50;
-// const dy = 50;
-
-// function clearDirection(baba) {
-//     baba.classList.remove("right");
-//     baba.classList.remove("down");
-//     baba.classList.remove("left");
-//     baba.classList.remove("up");
-// }
-
-// function handleHeadMove(e) {
-//     switch (e.key) {
-//         case KEY_RIGHT:
-//             if (head.classList.contains("left") || head.classList.contains("right")) {
-//                 break
-//             };
-//             clearDirection(head);
-//             head.classList.add("right")
-//             break
-//         case KEY_DOWN:
-//             if (head.classList.contains("up") || head.classList.contains("down")) {
-//                 break
-//             };
-//             clearDirection(head);
-//             head.classList.add("down")
-//             break
-//         case KEY_LEFT:
-//             if (head.classList.contains("right") || head.classList.contains("left")) {
-//                 break
-//             };
-//             clearDirection(head);
-//             head.classList.add("left")
-//             break
-//         case KEY_UP:
-//             if (head.classList.contains("down") || head.classList.contains("up")) {
-//                 break
-//             };
-//             clearDirection(head);
-//             head.classList.add("up")
-//             break
-//     }
-// }
+function handleHeadMove(e) {
+    switch (e.key) {
+        case KEY_RIGHT:
+            if (direction !== "left") {
+                direction = "right";
+            }
+            break
+        case KEY_DOWN:
+            if (direction !== "up") {
+                direction = "down";
+            }
+            break
+        case KEY_LEFT:
+            if (direction !== "right") {
+                direction = "left";
+            }
+            break
+        case KEY_UP:
+            if (direction !== "down") {
+                direction = "up";
+            }
+            break
+    }
+}
 
 // function snakeMove() {
     
@@ -79,5 +110,4 @@ form.addEventListener("submit", handleClear);
 
 // setInterval(snakeMove, 1000);
 
-// window.addEventListener("keydown", handleHeadMove);
 
